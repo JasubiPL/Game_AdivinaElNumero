84cd9f075db843d56f54e4bbd7c4b9572fe6ecbb
JasubiP <gameportal7@hotmail.com>
2023-01-28 00:45:02 -0600

    Se creo la pantalla de intro

---
+++ src/Pantallas/pantallaDeIntro.js
@@ -0,0 +1,20 @@
+import '../css/pantallaDeIntro.css';
+import logoJasubiP from '../img/imagotipo-jasubip.png';
+import spinnerIcon from '../img/spinner-icon.png';
+
+const pantallaDeIntro = document.getElementById("pantallaDeIntro");
+
+
+pantallaDeIntro.innerHTML = `
+    <div class="contenedorPantallaDeIntro">
+        <div class="infoPantallaDeIntroCenter">
+            <img class="imagotipo" src="${logoJasubiP}" />
+            <img class="spinner" src="${spinnerIcon}" />
+
+        </div>
+    </div>
+`;
+
+setTimeout(() => {
+    pantallaDeIntro.style.display = "none";
+},3000)
